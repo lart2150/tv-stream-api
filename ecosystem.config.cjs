960193e74b1918e2295d3f52ec0d7f941a0c2d99
Brian Engert <lart2150@gmail.com>
2024-09-14 10:42:37 -0500

    fix: update pm2 ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
     apps: [{
-        name: 'api',
-        script: `${process.env.PWD}/index.js`,
-        node_args: '--experimental-specifier-resolution=node',
+        name: 'tv-stream-api',
+        script: `dist/index.js`,
+        interpreter_args: '--experimental-specifier-resolution=node --tls-min-v1.0',
         watch: false,
         autorestart: true,
         restart_delay: 1000,
         kill_timeout: 3000,
         exec_mode: 'cluster',
-        instances: 0,
+        instances: 1,
         instance_var: 'INSTANCE_ID',
         env: {
             NODE_ENV: 'development',
